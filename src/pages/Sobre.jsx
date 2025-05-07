8f66e8fd868eb8de474c11e61dbaca2ad4176c7a
carlospph <intablete@gmail.com>
2025-05-07 01:47:00 -0300

    construir página sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,5 +1,11 @@
 export default function Sobre () {
   return(
-  <h1>Página sobre</h1>
+    <>
+    <h1>Página sobre</h1>
+  <p>
+
+    Estamos em desensenvolvmiemtnos nos últimos dias para que possamos lograr exito no desenvolvimento de sitesm , layouts e outros.
+  </p>
+  </>
   );
 }
